c95e7753abc8da426ac39ba6a1d7d81ca5f644cb
Schwegg <mikeschwender13@gmail.com>
2025-11-06 09:09:00 -0500

    Update LinkedEventQueue.gd

--- addons/lovely_event/scripts/queues/LinkedEventQueue.gd
+++ addons/lovely_event/scripts/queues/LinkedEventQueue.gd
@@ -10,6 +10,9 @@ func _init( queue_name : String, linked_node : Node, run_while_paused : bool = f
 
 
 func update( _dt : float ) -> void:
+	if is_essential:
+		push_error( "LinkedEventQueue cannot be essential! use EventQueue instead!" );
+		is_essential = false;
 	if link == null:
 		LovelyEvent.remove_queue( self );
 		return;
